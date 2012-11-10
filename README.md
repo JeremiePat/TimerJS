e24b3aaa7bf13cb58771ed9236bbe57f47ee40fe
Jeremie Patonnier <jeremie.patonnier@gmail.com>
2012-11-11 00:32:05 +0100

    First commit: Base library + documentation

--- README.md
+++ README.md
@@ -1,4 +1,294 @@
 TimerJS
 =======
 
-Simple library to manage time based animations
+A simple library to manage time based animations
+
+There is nothing more unpleasant than playing with timestamps to perform an animation, especially when you want to do some difficult computation such as playing the animation backward or applying an easing fonction to the value you want to animate. TimerJS is a solution to all that mess.
+
+TimerJS offer you a passive timer which give you a simple 0 to 1 value all along the animation time. This value can easily use as a multiplicator to any value you want to animate.
+
+```javascript
+// Define a new timer for an animation of 5000ms
+var timer = new Timer(5000);
+
+// Define the animation loop
+function myAnimation() {
+    // Use timer.position.value to change the width of an element
+    // Based on the time from the moment the timer has starded to play.
+    myObject.style.width = (timer.position.value * 100).toFixed(2) + "%";
+
+    // The animation loop can be automaticaly stoped once 
+    // the Timer reach the end of the animation time.
+    if(timer.is.playing) {
+        requestAnimationFrame(myAnimation);
+    }
+}
+
+// Start the timer
+Timer.play();
+
+// Start the animation loop, independenly from the timer
+myAnimation();
+```
+
+API Documentation
+-----------------
+
+### Constructor
+
+``Timer(duration, delay, easing)``
+
+When you instentiate an new Timer, you can immediatly set its duration, its delay and the easing function to apply.
+
+See below to know more about each of this parameters
+
+```javascript
+// This will instentiate a 0ms timer
+var timer = new Timer();
+
+// This will instentiate a 5000ms timer
+var timer = new Timer(5000);
+
+// This will instentiate a 5000ms timer with a 1000ms delay
+var timer = new Timer(5000, 1000);
+
+// This will instentiate a 5000ms timer with the easeInQuad build-in easing function
+var timer = new Timer(5000, 0, 'easeInQuad');
+
+// This will instentiate a 5000ms timer with a custom easing function
+var timer = new Timer(5000, 0, function (t) { return t*t; });
+
+```
+
+### Timer.play
+
+``Timer.play(speedFactor)``
+
+The play fonction launch the timer or change its speed. 
+
+The speedFactor argument determine de speed at which the timer is walk along the time line. If the speedFactor argument is homited, it's like the value ``1`` was used.
+
+The speedFactor can be negative. In that case, the timer is playing backward.
+
+```javascript
+var timer = new Timer(5000);
+
+timer.play();   // The timer start playing at its normal speed
+timer.play(0);  // The timer is paused
+timer.play(-1); // The timer is playing backward
+timer.play(2);  // The timer is playing twice its normal speed
+```
+
+### Timer.stop
+
+``Timer.stop()``
+
+The stop function reinitialize the timer to it's starting position.
+
+### Timer.position
+
+The ``position`` property is a readonly object with two properties: ``value``and ``time``. The two values give the current position of the time and value in the range 0-1 since the timer has started to play. A time value of 0 means that the time is at the begining of the time line. A time value of 1 means that the time is at the end of the time line. The progression of the ``time`` value is always linear where the progression of the ``value`` value depend on the easing function used by the timer (linear by default).
+
+```javascript
+var timer = new Timer(5000);
+
+timer.play();
+
+setTimeout(function () {
+    console.log(timer.position.value); // 0.5
+    console.log(timer.position.time);  // 0.5
+}, 2500);
+```
+
+### Timer.is
+
+The ``is`` property is a readonly object with two boolean properties: ``playing`` and ``paused``. The first one say if the timer is playing and the second if it is paused.
+
+This property is a readonly property.
+
+```javascript
+var play, pause,
+    timer = new Timer(5000);
+
+play  = timer.is.playing // false
+pause = timer.is.paused  // false
+
+timer.play();
+
+play  = timer.is.playing // true
+pause = timer.is.paused  // false
+
+timer.play(0);
+
+play  = timer.is.playing // true
+pause = timer.is.paused  // true
+
+timer.stop();
+
+play  = timer.is.playing // false
+pause = timer.is.paused  // false
+```
+
+### Timer.speed
+
+The ``speed`` property give you the speed factor of the timer.
+
+This property is a readonly property.
+
+```javascript
+var speed,
+    timer = new Timer(5000);
+
+speed = timer.speed // 1
+
+timer.play();
+speed = timer.speed // 1
+
+timer.play(0);
+speed = timer.speed // 0
+
+timer.play(2);
+speed = timer.speed // 2
+
+timer.play(-1);
+speed = timer.speed // -1
+```
+
+### Timer.easing
+
+The ``easing`` property is the easing function used by the timer to compute the value of the ``position`` property. If the timer is playing, this property is a readonly property. Otherwise, you can change the easing function as you wish.
+
+To change the easing function you have two possibilities.
+
+* Creating a custom easing function. That function will accept an argument representing the time in a 0-1 range.
+* Using one of the following keywords:
+  * ``linear``
+  * ``easeInQuad``
+  * ``easeOutQuad``
+  * ``easeInOutQuad``
+  * ``easeInCubic``
+  * ``easeOutCubic``
+  * ``easeInOutCubic``
+  * ``easeInQuart``
+  * ``easeOutQuart``
+  * ``easeInOutQuart``
+  * ``easeInQuint``
+  * ``easeOutQuint``
+  * ``easeInOutQuint``
+  * ``easeInSine``
+  * ``easeOutSine``
+  * ``easeInOutSine``
+  * ``easeInExpo``
+  * ``easeOutExpo``
+  * ``easeInOutExpo``
+  * ``easeInCirc``
+  * ``easeOutCirc``
+  * ``easeInOutCirc``
+  * ``easeInElastic``
+  * ``easeOutElastic``
+  * ``easeInOutElastic``
+  * ``easeInBack``
+  * ``easeOutBack``
+  * ``easeInOutBack``
+  * ``easeInBounce``
+  * ``easeOutBounce``
+  * ``easeInOutBounce``
+
+```javascript
+var timer = new Timer(5000);
+
+timer.easing = "easeInQuad";
+
+timer.easing = function (t) { 
+    return t*t; 
+};
+```
+
+### Timer.startTime
+
+The ``startTime`` property give you the timestamp of the begining of the animation. If the animation hasn't started yet, this property return ``null``;
+
+This property is a readonly property.
+
+```javascript
+var startTime,
+    timer = new Timer(5000);
+
+startTime = timer.startTime // null
+
+timer.play();
+
+startTime = timer.startTime // A timestamp
+
+timer.stop();
+
+startTime = timer.startTime // null
+
+```
+
+### Timer.duration
+
+The ``duration`` property allow you to retrieve the timer duration. If the timer is playing, this property is a readonly property. If the timer is not playing, you can change the value of this property.
+
+```javascript
+var duration,
+    timer = new Timer(5000);
+
+duration = timer.duration; // 5000
+
+timer.duration = 1000; // The timer is now set with a 1000ms duration
+
+timer.play();
+
+duration = timer.duration; // 1000
+
+timer.duration = 2000; // Throw an error
+```
+
+### Timer.delay
+
+The ``delay`` property allow you to retrieve the timer delay. If the timer is playing, this property is a readonly property. If the timer is not playing, you can change the value of this property.
+
+```javascript
+var delay,
+    timer = new Timer(5000);
+
+delay = timer.delay; // 0
+
+timer.delay = 1000; // The timer is now set with a 1000ms delay
+
+timer.play();
+
+delay = timer.delay; // 1000
+
+timer.delay = 2000; // Throw an error
+```
+
+A delay can be negative. A positive delay means that the timer will wait for the delay value before changing its time and value position. A negative delay means that when the timer will start it will be as if it was playing since the delay value.
+
+```javascript
+var value, time,
+    timer = new Timer(5000, -2500);
+
+value = timer.position.value; // 0
+time  = timer.position.time ; // 0
+
+timer.play();
+
+value = timer.position.value; // 0.5
+time  = timer.position.time ; // 0.5
+```
+
+
+Roadmap
+-------
+
+This timer is far from finished the plan is to add the following features:
+
+* Enable loop
+* Autorized "out of range" time (currently constrain to the a 0-1 range)
+* Improve reverse animation
+* Add the ability to remember the last state of an animation when stoped.
+* Add the ability to get a value against an arbitrary time, even if the timer is not playing.
+* Allow change of any paramaters while the timer is playing
+* Allow to synchronize several timers
---
+++ timer.js
@@ -0,0 +1,539 @@
+/*
+    Copyright (c) 2012
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+(function (window) {
+    "use strict";
+
+    // ------------------ //
+    // UTILITIES          //
+    // ------------------ //
+    
+    function isNumber(value) {
+        return Object.prototype.toString.call(value) === '[object Number]';
+    }
+
+    function isFunction(value) {
+        return Object.prototype.toString.call(value) === '[object Function]';
+    }
+
+    function isNaN(value) {
+        return isNumber(value) && value !== +value;
+    }
+
+    function toInt(value, alt) {
+        return isNaN(+value) ? alt : +value; 
+    }
+
+    function toPosInt(value) {
+        return +value >= 0 ? +value : 0;
+    }
+
+    function toNullTime(isNull) {
+        if (isNull === null)  { return null; }
+        if (isNumber(isNull)) { return isNull; }
+        return +new Date();
+    }
+
+    function Easing(name) {
+        this.easeFx = (isFunction(name) && name) || this.func[name] || this.func.linear;
+    }
+    
+    Easing.prototype = {
+        getValue : function (begin, end, now) {
+            if (begin === end) {
+                throw 'A zero time animation as no meaning!';
+            }
+
+            var time = (now - begin) / (end - begin);
+
+            return this.easeFx(time, 0, 1, 1);
+        },
+
+        getTime  : function (begin, end, now) {
+            if (begin === end) {
+                throw 'A zero time animation as no meaning!';
+            }
+
+            return (now - begin) / (end - begin);
+        },
+
+        func : {
+            // t : current time: 0 < t < 1 sur l'axe du temps lineaire
+            // b : start value : 0 sur l'axe de la valeur
+            // c : end value   : 1 sur l'axe de la valeur
+            // d : duration    : 1 sur l'axe du temps
+            
+            linear: function linear(t) {
+                return t;
+            },
+
+            easeInQuad: function easeInQuad(t) {
+                return t*t;
+            },
+            easeOutQuad: function easeOutQuad(t) {
+                return -1*t*(t-2);
+            },
+            easeInOutQuad: function easeInOutQuad(t) {
+                if ((t/=1/2) < 1) { return 1/2*t*t; }
+                return -1/2 * ((--t)*(t-2) - 1);
+            },
+            easeInCubic: function easeInCubic(t) {
+                return t*t*t;
+            },
+            easeOutCubic: function easeOutCubic(t) {
+                return (t-=1)*t*t + 1;
+            },
+            easeInOutCubic: function easeInOutCubic(t) {
+                if ((t/=1/2) < 1) { return 1/2*t*t*t; }
+                return 1/2*((t-=2)*t*t + 2);
+            },
+            easeInQuart: function easeInQuart(t) {
+                return t*t*t*t;
+            },
+            easeOutQuart: function easeOutQuart(t) {
+                return -1 * ((t-=1)*t*t*t - 1);
+            },
+            easeInOutQuart: function easeInOutQuart(t) {
+                if ((t/=1/2) < 1) { return 1/2*t*t*t*t; }
+                return -1/2 * ((t-=2)*t*t*t - 2);
+            },
+            easeInQuint: function easeInQuint(t) {
+                return t*t*t*t*t;
+            },
+            easeOutQuint: function easeOutQuint(t) {
+                return (t-=1)*t*t*t*t + 1;
+            },
+            easeInOutQuint: function easeInOutQuint(t) {
+                if ((t/=1/2) < 1) { return 1/2*t*t*t*t*t; }
+                return 1/2*((t-=2)*t*t*t*t + 2);
+            },
+            easeInSine: function easeInSine(t) {
+                return -1 * Math.cos(t * (Math.PI/2)) + 1;
+            },
+            easeOutSine: function easeOutSine(t) {
+                return Math.sin(t*Math.PI/2);
+            },
+            easeInOutSine: function easeInOutSine(t) {
+                return -1/2 * (Math.cos(t*Math.PI) - 1);
+            },
+            easeInExpo: function easeInExpo(t) {
+                return (t===0) ? 0 : Math.pow(2, 10 * (t - 1));
+            },
+            easeOutExpo: function easeOutExpo(t) {
+                return (t===1) ? 1 : 1 - Math.pow(2, -10 * t);
+            },
+            easeInOutExpo: function easeInOutExpo(t) {
+                if (t===0) { return 0; }
+                if (t===1) { return 1; }
+                if ((t/=1/2) < 1) { return 1/2 * Math.pow(2, 10 * (t - 1)); }
+                return 1/2 * (2 - Math.pow(2, -10 * --t));
+            },
+            easeInCirc: function easeInCirc(t) {
+                return -1 * (Math.sqrt(1 - t*t) - 1);
+            },
+            easeOutCirc: function easeOutCirc(t) {
+                return Math.sqrt(1 - (t-=1)*t);
+            },
+            easeInOutCirc: function easeInOutCirc(t) {
+                if ((t/=1/2) < 1) { return -1/2 * (Math.sqrt(1 - t*t) - 1); }
+                return 1/2 * (Math.sqrt(1 - (t-=2)*t) + 1);
+            },
+            easeInElastic: function easeInElastic(t) {
+                var p=0.3, s=p/(2*Math.PI) * Math.asin(1);
+                if (t===0) { return 0; }
+                if (t===1) { return 1; }
+                return -(Math.pow(2,10*(t-=1)) * Math.sin( (t-s)*(2*Math.PI)/p ));
+            },
+            easeOutElastic: function easeOutElastic(t) {
+                var p=0.3, s=p/(2*Math.PI) * Math.asin(1);
+                if (t===0) { return 0; }
+                if (t===1) { return 1; }
+                return Math.pow(2,-10*t) * Math.sin( (t-s)*(2*Math.PI)/p ) + 1;
+            },
+            easeInOutElastic: function easeInOutElastic(t) {
+                var p=1.5*0.3, s=p/(2*Math.PI) * Math.asin(1);
+                if (t===0) {return 0;}
+                if ((t*=2)===2) {return 1;}
+                if (t < 1) {return -0.5*(Math.pow(2,10*(t-=1)) * Math.sin((t-s)*(2*Math.PI)/p));}
+                return Math.pow(2,-10*(t-=1)) * Math.sin( (t-s)*(2*Math.PI)/p )*0.5 + 1;
+            },
+            easeInBack: function easeInBack(t) {
+                var s=1.70158;
+                return t*t*((s+1)*t - s);
+            },
+            easeOutBack: function easeOutBack(t) {
+                var s=1.70158;
+                return (t-=1)*t*((s+1)*t + s) + 1;
+            },
+            easeInOutBack: function easeInOutBack(t) {
+                var s=1.70158;
+                if ((t/=1/2) < 1) { return 1/2*(t*t*(((s*=(1.525))+1)*t - s)); }
+                return 1/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2);
+            },
+            easeInBounce: function easeInBounce(t) {
+                return 1 - Easing.prototype.func.easeOutBounce (1-t);
+            },
+            easeOutBounce: function easeOutBounce(t) {
+                var a=2.625, b=0.984375;
+                if (t < (1/2.75)) { a=0; b=0; } 
+                else if (t < (2/2.75)) { a=1.5; b=0.75; } 
+                else if (t < (2.5/2.75)) { a=2.25; b=0.9375; }
+                return 7.5625*(t-=(a/2.75))*t + b;
+            },
+            easeInOutBounce: function easeInOutBounce(t, b, c, d) {
+                if (t < 1/2) { return Easing.prototype.func.easeInBounce (t*2) * 0.5; }
+                return Easing.prototype.func.easeOutBounce (t*2-1) * 0.5 + 0.5;
+            }
+        }
+    };
+
+    // ------------------ //
+    // CORE               //
+    // ------------------ //
+
+    function Timer(dur, delay, easing) {
+        // Sefely keep the internal properties of the object
+        var closed = {
+            // Specific data for users
+            userDelay : {
+                value : 0,
+                set   : function (value) {
+                    this.value = toInt(value, 0);
+                }
+            },
+
+            userTime  : {
+                value : null,
+                set   : function (isNull) {
+                    this.value = toNullTime(isNull);
+                    closed.startTime.set(isNull);
+                }
+            },
+
+            // Keep the first time the Timer is played
+            startTime : {
+                value : null,
+                set   : function (isNull) {
+                    this.value = toNullTime(isNull);
+                    
+                    // When the value is set (mening, the Timer start)
+                    // The pause and back times MUST be reinitialized
+                    closed.pauseTime.value = null;
+                    closed.backTime.value = null;
+                    closed.begin.set();
+                    closed.end.set();
+                }
+            },
+
+            // Keep the last time the Timer is paused
+            pauseTime : {
+                value : null,
+                set   : function (isNull) {
+                    closed.pauseTime.value = toNullTime(isNull);
+                }
+            },
+
+            // Keep the last time the Timer is going backward
+            backTime : {
+                value : null,
+                set   : function (isNull) {
+                    closed.backTime.value = toNullTime(isNull);
+                }
+            },
+
+            delay : {
+                value : 0,
+                set   : function (value) {
+                    this.value = toInt(value, 0);
+                    closed.begin.set();
+                    closed.end.set();
+                }
+            },
+
+            duration : {
+                value : 0,
+                set   : function (value) {
+                    this.value = toPosInt(value);
+                    closed.end.set();
+                }
+            },
+
+            easing : {
+                value: new Easing('linear'),
+                set : function (value) {
+                    this.value = new Easing(value);
+                }
+            },
+
+            speedFactor : {
+                value : 1,
+                set : function (value) {
+                    this.value = toInt(value, 1);
+                    closed.end.set();
+                }
+            },
+
+            begin : {
+                value : 0,
+                set : function () {
+                    this.value = closed.startTime.value
+                               + closed.userDelay.value
+                               + closed.delay.value;
+                }
+            },
+
+            end : {
+                value : 0,
+                set : function () {
+                    var sf = closed.speedFactor.value;
+                    sf *= sf < 0 ? -1 : 1;
+
+                    closed.end.value = closed.startTime.value
+                                     + closed.userDelay.value
+                                     + closed.delay.value
+                                     + (sf === 0 ? closed.duration.value 
+                                                 : closed.duration.value / sf);
+                }
+            }
+        };
+
+        // Accessor to closed statement
+        // You use those methods at your own risk
+
+        Object.defineProperty(this, "set", {
+            value : function(property, value) {
+                closed[property].set(value);
+                return closed[property].value;
+            },
+            writable     : false,
+            enumerable   : false,
+            configurable : false
+        });
+
+        Object.defineProperty(this, "get", {
+            value : function(property) {
+                return closed[property].value;
+            },
+            writable     : false,
+            enumerable   : false,
+            configurable : false
+        });
+
+        // Truly initialize the object
+        this.set("duration",  dur);
+        this.set("userDelay", delay);
+        this.set("easing",    easing);
+    }
+
+    // ------------------------- //
+    // PROPERIES
+    // ------------------------- //
+    // Timer.startTime
+    Object.defineProperty(Timer.prototype, "startTime", {
+        set : function () {
+            throw "Timer.startTime is a readonly property";
+        },
+        get : function () {
+            return this.get("userTime");
+        },
+        enumerable   : true,
+        configurable : false
+    });
+
+    // Timer.delay
+    Object.defineProperty(Timer.prototype, "delay", {
+        set : function (value) {
+            if(this.is.playing){
+                throw "Timer.delay can not be set while it's playing";
+            }
+
+            this.set("userDelay", value);
+        },
+        get : function () {
+            return this.get("userDelay");
+        },
+        enumerable   : true,
+        configurable : false
+    });
+
+    // Timer.speed
+    Object.defineProperty(Timer.prototype, "speed", {
+        set : function () {
+            throw "Timer.speed is a read only property";
+        },
+        get : function () {
+            return this.get("speedFactor");
+        },
+        enumerable   : true,
+        configurable : false
+    });
+
+    // Timer.duration
+    Object.defineProperty(Timer.prototype, "duration", {
+        set : function (value) {
+            if(this.is.playing) {
+                throw "Timer.duration can not be set while it's playing";
+            }
+
+            this.set("duration", value);
+        },
+        get : function () {
+            return this.get("duration");
+        },
+        enumerable   : true,
+        configurable : false
+    });
+
+    // Timer.easing
+    Object.defineProperty(Timer.prototype, "easing", {
+        set : function (value) {
+            if(this.is.playing) {
+                throw "Timer.easing can not be set while it's playing";
+            }
+            
+            this.set("easing", value);
+        },
+        get : function () {
+            return this.get("easing").easeFx;
+        },
+        enumerable   : true,
+        configurable : false
+    });
+
+    // Timer.is
+    //         .playing
+    //         .paused
+    Object.defineProperty(Timer.prototype, "is", {
+        set : function () {
+            throw "Timer.is is a readonly property";
+        },
+        get : function () {
+            var startTime   = this.get("startTime"),
+                speedFactor = this.get("speedFactor"),
+                output      = {
+                    playing: startTime !== null,
+                    paused : startTime !== null && speedFactor === 0
+                };
+
+            if (speedFactor > 0 && this.position.time >= 1) { this.stop(); }
+            if (speedFactor < 0 && this.position.time <= 0) { this.stop(); }
+
+            return output;
+        },
+        enumerable   : true,
+        configurable : false
+    });
+
+    //Timer.position
+    Object.defineProperty(Timer.prototype, "position", {
+        set : function () {
+            throw "Timer.position is a readonly property";
+        },
+        get : function () {
+            var begin, end, now, backTime, ease, speedFactor,
+                startTime   = this.get("startTime"),
+                output      = {
+                    value : 0,
+                    time  : 0
+                };
+
+            if (startTime === null) { return output; }
+
+            speedFactor = this.get("speedFactor");
+            now = +new Date();
+
+            if (speedFactor === 0) {
+                now = this.get("pauseTime");
+            }
+            else if (speedFactor < 0) { 
+                backTime = this.get("backTime");
+                now = backTime*2 - now; 
+            } 
+
+            begin = this.get("begin");
+            end   = this.get("end");
+
+            if (now <= begin) { return output; }
+            if (now >= end)   { return {value: 1, time: 1 }; }
+
+            ease = this.get("easing");
+
+            output = {
+                value: ease.getValue(begin, end, now),
+                time : ease.getTime(begin, end, now)
+            };
+
+            return output;
+        },
+        enumerable   : true,
+        configurable : false
+    });
+
+    // ------------------------- //
+    // METHOD
+    // ------------------------- //
+    Timer.prototype.play = function (speedFactor) {
+        var shift, factor,
+            userTime     = this.get("userTime"),
+            delay        = this.get("delay"),
+            pauseTime    = this.get("pauseTime"),
+            backTime     = this.get("backTime"),
+            change       = this.get("userDelay"),
+            time         = this.position.time,
+            now          = +new Date();
+
+        if (userTime === null) {
+            userTime = this.set("userTime", now);
+            change   = 0;
+        }
+
+        if (pauseTime !== null) {
+            delay = this.set("delay", delay + (now - pauseTime));
+            this.set("pauseTime",  null);
+        }
+
+        if (backTime !== null) {
+            delay = this.set("delay", delay + (now - backTime)*2);
+            this.set("backTime",  null);
+        }
+
+        speedFactor  = this.set("speedFactor", speedFactor);
+
+        if (speedFactor === 0) { this.set("pauseTime", now); }
+        if (speedFactor   < 0) { this.set("backTime",  now); }
+
+        speedFactor *= speedFactor < 0 ? -1 : 1;
+        
+        shift  = now - userTime - delay - change;
+        factor = speedFactor === 0 ? this.get("duration")*time
+                                   : this.get("duration")*time/speedFactor;
+            
+        this.set("delay", delay + shift - factor);
+    };
+
+    Timer.prototype.stop = function() {
+        this.set("userTime",    null);
+        this.set("delay",       0);
+        this.set("speedFactor", 1);
+    };
+
+    window.Timer = Timer;
+})(this);
